ff48c8feb557430569e46c5b7e9d00358dc7c9a8
Kevin Alvarado <kevinju92@outlook.com>
2017-03-09 12:29:27 -0500

    avance tabla Listado de Items rentados

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,9 +5,17 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
+import edu.eci.pdsw.samples.entities.ItemRentado;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 /**
@@ -18,9 +26,40 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class AlquilerItemsBean implements Serializable {
 
-    ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
+    ServiciosAlquiler sp;
+    private Map<Integer,ItemRentado> ItemsRentados;
+    private ArrayList<String[]> listaPendientes;
+    private Cliente clienteSelected;
+    //@ManagedProperty(value = "#{ClientesBen}")
+    //private ClientesBean cb;
 
     public AlquilerItemsBean() {
+        sp = ServiciosAlquiler.getInstance();
+        //clienteSelected=cb.getClienteSeleccionado();
+        ItemsRentados= new HashMap<>();
     }
 
+    public ArrayList<String[]> getListaPendientes() {
+        ItemRentado itemTMP=null;
+        LocalDate fechaInicial=null;
+        LocalDate fechaEntrega=null;
+        long diasRestantes=0;
+        listaPendientes=new ArrayList<String[]>();
+        for(int i=0;i<listaPendientes.size();i++){
+            /*
+        this.fechainiciorenta = fechainiciorenta;
+        this.fechafinrenta = fechafinrenta;
+            */
+            itemTMP=ItemsRentados.get(i);
+            
+            fechaInicial=itemTMP.getFechainiciorenta().toLocalDate();
+            fechaEntrega=itemTMP.getFechafinrenta().toLocalDate();
+            diasRestantes=ChronoUnit.DAYS.between(fechaInicial, fechaEntrega);
+            
+            String[] tmp={itemTMP.getItem().getNombre(),Integer.toString((int) diasRestantes)};
+            listaPendientes.add(tmp);
+        }
+        return listaPendientes;
+    }
+    
 }
