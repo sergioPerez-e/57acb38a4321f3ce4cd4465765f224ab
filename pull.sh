25c9d2468a70daa45593eac8cb4d1606ca8a87a0
sergio <dark07perez@gmail.com>
2017-03-14 23:09:04 -0500

    Titulos paginas agregados :)

---
+++ pull.sh
@@ -0,0 +1,6 @@
+echo Debio haber commitado primero!!!  comite, add .., commit -m
+git pull --rebase https://github.com/dark07perez/57acb38a4321f3ce4cd4465765f224ab 
+git pull origin master
+git pull origin master
+git push origin master
+echo Pull, Pull --rebase y push Finalizado!!!
---
+++ push.sh
@@ -0,0 +1,3 @@
+echo Debio haber commitado primero!!!  comite, add .., commit -m
+git push origin master
+echo push Finalizado!!!
